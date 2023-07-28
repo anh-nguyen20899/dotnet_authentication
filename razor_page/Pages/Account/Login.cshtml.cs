52b848b6f30672f2612ba074c745a9b4e8f37640
anhng <anananh1999@gmail.com>
2023-07-28 10:16:51 +1000

    Init razor web page

---
+++ razor_page/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_page.Pages.Account;
+public class LoginModel : PageModel
+{
+    [BindProperty]
+    public string? Username {get; set;}
+    [BindProperty]
+    public string? Password {get; set;}
+
+    public async Task<IActionResult> OnPost()
+    {
+        if(!ModelState.IsValid) return Page();
+        // Verify the Credential
+            if(Username == "admin" && Password == "password")
+            {
+                // Create Security Context
+                var claims = new List<Claim> 
+                { 
+                    new Claim(ClaimTypes.Name,"admin"),
+                    new Claim(ClaimTypes.Email,"admin@mywebsite.com"),
+                };
+                var identity = new ClaimsIdentity(claims,"MyCookieAuth");
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("MyCookieAuth", principal);
+                return RedirectToPage("/index");
+            }
+            
+        return RedirectToPage("/index");
+    }
+}
+public class Credential 
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public string? Username;
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password;
+}
